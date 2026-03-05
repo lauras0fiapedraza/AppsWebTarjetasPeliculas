eddfd651f20c7dc8043750a1852806251db10cca
SofiaPedraza <lauritrujillo123@gmail.com>
2026-03-04 20:19:59 -0500

    array de datos de las peliculas

---
+++ src/data/peliculas.js
@@ -0,0 +1,53 @@
+//archivo para array con los datos de las peliculas
+const peliculas = [
+    {
+        id: 1,
+        titulo: "Interstellar",
+        anio: 2014,
+        genero: "ciencia ficcion",
+        calificación: 9.2,
+        disponible: true,
+    },
+    {
+        id: 2,
+        titulo: "El Padrino",
+        anio: 1972,
+        genero: "Drama",
+        calificacion: 9.5,
+        disponible: false,
+    },
+    {
+        id: 3,
+        titulo: "Parasite",
+        anio: 2019,
+        genero: "Thriller",
+        calificacion: 8.6,
+        disponible: true,
+    },
+    {
+        id: 4,
+        titulo: "The Dark Knight",
+        anio: 2008,
+        genero: "Acción",
+        calificacion: 9.0,
+        disponible: true,
+    },
+    {
+        id: 5,
+        titulo: "Forrest Gump",
+        anio: 1994,
+        genero: "Drama",
+        calificacion: 8.8,
+        disponible: false,
+    },
+    {
+        id: 6,
+        titulo: "Inception",
+        anio: 2010,
+        genero: "Ciencia Ficción",
+        calificacion: 8.7,
+        disponible: true,
+    },
+];
+
+export default peliculas;
